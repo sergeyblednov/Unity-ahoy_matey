f3c4964a96f588471c395cd01d5b88c961e12942
Sergey Blednov <sergey.blednov@pixacore.com>
2017-05-23 11:26:38 -0400

    Initial commit

---
+++ Assets/MyNetworkManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MyNetworkManager : NetworkManager {
+
+	public void MyStartHost () {
+		StartHost ();
+		Debug.Log (Time.timeSinceLevelLoad + " Starting Host at ");
+	}
+
+	public override void OnStartHost() {
+		Debug.Log ( Time.timeSinceLevelLoad + "Host started");
+	}
+
+	public override void OnStartClient (NetworkClient client) {
+		Debug.Log (Time.timeSinceLevelLoad + " Client start requested.");
+		InvokeRepeating ("PrintDot", 0, 1f);
+	}
+
+	public override void OnClientConnect (NetworkConnection conn) {
+		Debug.Log (Time.timeSinceLevelLoad + " Client is connected to IP: " + conn.address);
+		CancelInvoke ();
+	}
+
+	void PrintDot () {
+		print (".");
+	}
+}
---
+++ Assets/Players/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine.Networking;
+
+public class Player : NetworkBehaviour {
+
+	private Vector3 inputValue;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public override void OnStartLocalPlayer () {
+		GetComponentInChildren<Camera> ().enabled = true;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (!isLocalPlayer) {
+			return;
+		}
+
+		inputValue.x = CrossPlatformInputManager.GetAxis ("Horizontal");
+		inputValue.y = 0;
+		inputValue.z = CrossPlatformInputManager.GetAxis ("Vertical");
+		transform.Translate (inputValue);
+	}
+}
